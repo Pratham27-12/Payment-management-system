1cbbb15531460d1458332573bbb7a4bed8193dda
prathamgolwala <prathamgo@zeta.tech>
2025-08-14 11:55:23 +0530

    Updated DDl script for db_migration

--- MiniProject2/src/main/resources/config.db_migrations/V1.0.0_added_payment_details_table.sql
+++ MiniProject2/src/main/resources/config.db_migrations/V1.0.0_added_payment_details_table.sql
@@ -5,15 +5,15 @@ CREATE TYPE payment_status AS ENUM ('PENDING', 'PROCESSING', 'COMPLETED');
 CREATE TYPE payment_type AS ENUM ('INCOMING','OUTGOING');
 
 CREATE TABLE IF NOT EXISTS payment_system.payment_details (
-    payment_id varchar(50) NOT NULL,
+    payment_id bigint NOT NULL,
     amount varchar(100) NOT NULL,
     revision_count bigint NOT NULL DEFAULT 1,
     currency varchar(3) NOT null,
-    payment_type payment_type NOT NULL,
-    category payment_category NOT NULL,
+    payment_type varchar(20) NOT NULL,
+    category varchar(20) NOT NULL,
     created_by varchar(30) NOT NULL,
     account_name varchar(30) NOT NULL,
-    status  payment_status NOT NULL,
+    status  varchar(20) NOT NULL,
     created_at bigint NOT NULL DEFAULT (EXTRACT(epoch FROM now()) * 1000::numeric),
     updated_at bigint NOT NULL DEFAULT (EXTRACT(epoch FROM now()) * 1000::numeric),
     CONSTRAINT payment_pkey PRIMARY KEY (payment_id)
